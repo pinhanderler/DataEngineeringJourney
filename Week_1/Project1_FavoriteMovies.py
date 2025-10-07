ad072732ed1a7751c56f9cc6b2d412a3b8610d96
Pinhan <pinhanderler@gmail.com>
2025-10-07 18:42:37 +0200

    Update Project1_FavoriteMovies.py

--- Week_1/Project1_FavoriteMovies.py
+++ Week_1/Project1_FavoriteMovies.py
@@ -1,1 +1,15 @@
+# Project 1: Favorite Movie List 
+
+print("Enter your 3 favorite movies:")
+
+movie1 = input("Movie 1: ")
+movie2 = input("Movie 2: ")
+movie3 = input("Movie 3: ")
+
+movies = [movie1, movie2, movie3]
+
+print("\n Your movie list:", movies)
+print("First movie:", movies[0])
+print("Last movie:", movies[-1])
+print("Number of movies:", len(movies))
 
